00540f26c42dbedc5a885520181f4b04d5589f63
Li, ChungHao <chung@stemworldeducationalservices.com>
2022-09-25 19:47:58 -0400

    Chapter4 - Complete shaking head part

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+
+    public Rigidbody head;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,18 @@ public class PlayerController : MonoBehaviour
             Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+ 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            // TODO
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
