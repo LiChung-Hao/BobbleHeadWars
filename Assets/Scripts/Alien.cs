bf52d06f665d2583bec0958149bad109bcbd38af
Li, ChungHao <chung@stemworldeducationalservices.com>
2022-10-09 16:31:52 -0400

    Chapter8 - Add event listener for alien death

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 
 public class Alien : MonoBehaviour
 {
     public Transform target;
     public float navigationUpdate;
+    public UnityEvent OnDestroy;
+
     private float navigationTime = 0;
     private NavMeshAgent agent;
     // Start is called before the first frame update
@@ -33,7 +36,14 @@ public class Alien : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        Die();
         SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+    }
+
+    public void Die()
+    {
+        OnDestroy.Invoke();
+        OnDestroy.RemoveAllListeners();
+        Destroy(gameObject);
     }
 }
