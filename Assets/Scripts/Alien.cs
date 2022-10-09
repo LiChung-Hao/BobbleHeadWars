82fe69b05ba32428763516c7274b38d930a82762
Li, ChungHao <chung@stemworldeducationalservices.com>
2022-10-09 18:02:50 -0400

    Chapter8 - Add alien head disambled feature

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -10,6 +10,8 @@ public class Alien : MonoBehaviour
     public Transform target;
     public float navigationUpdate;
     public UnityEvent OnDestroy;
+    public Rigidbody head;
+    public bool isAlive = true;
 
     private float navigationTime = 0;
     private NavMeshAgent agent;
@@ -32,18 +34,36 @@ public class Alien : MonoBehaviour
                 navigationTime = 0;
             }
         }
-        
+        if (isAlive)
+        {
+            //...
+        }
+
     }
     void OnTriggerEnter(Collider other)
     {
-        Die();
-        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        if (isAlive)
+        {
+            Die();
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+        }
     }
 
     public void Die()
     {
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+
         OnDestroy.Invoke();
         OnDestroy.RemoveAllListeners();
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+
+        head.GetComponent<SelfDestruct>().Initiate();
         Destroy(gameObject);
     }
 }
