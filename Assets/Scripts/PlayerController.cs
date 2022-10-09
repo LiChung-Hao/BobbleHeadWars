2f8ff2b519d32ce2c1c1e601ee2bb2f204ef0623
Li, ChungHao <chung@stemworldeducationalservices.com>
2022-10-09 16:47:52 -0400

    Chapter8 - Add player gets hit feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     public Rigidbody head;
     public LayerMask layerMask;
     public Animator bodyAnimator;
+    public float[] hitForce;
+    public float timeBetweenHits = 2.5f;
+
+
+    private bool isHit = false;
+    private float timeSinceHit = 0;
+    private int hitNumber = -1;
 
     private CharacterController characterController;
     private Vector3 currentLookTarget = Vector3.zero;
@@ -24,6 +31,16 @@ public class PlayerController : MonoBehaviour
             Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+
+        if (isHit)
+        {
+            timeSinceHit += Time.deltaTime;
+            if (timeSinceHit > timeBetweenHits)
+            {
+                isHit = false;
+                timeSinceHit = 0;
+            }
+        }
     }
     void FixedUpdate()
     {
@@ -59,6 +76,31 @@ public class PlayerController : MonoBehaviour
             // 3
             transform.rotation = Quaternion.Lerp(transform.rotation,
              rotation, Time.deltaTime * 10.0f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Alien alien = other.gameObject.GetComponent<Alien>();
+        if (alien != null)
+        { // 1
+            if (!isHit)
+            {
+                hitNumber += 1; // 2
+                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
+                if (hitNumber < hitForce.Length) // 3 
+                {
+                    cameraShake.intensity = hitForce[hitNumber];
+                    cameraShake.Shake();
+                }
+                else
+                {
+                    // death todo
+                }
+                isHit = true; // 4
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
+            }
+            alien.Die();
         }
     }
 }
